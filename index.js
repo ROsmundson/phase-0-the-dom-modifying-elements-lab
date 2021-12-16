9431f6d5d0457e98b6e9455e27dcbfa0927015b7
Ryan Osmundson <rosmundson@outlook.com>
2021-12-16 10:55:33 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your code here!
+// // Write your code here!
+main.remove()
+
+// const newHeader = document.createElement('h1') //create variable and assign it to new element
+// newHeader.id = 'victory'//give element an id
+// newHeader.append('h1')
+
+// const sweetMessage = document.createElement("p")
+// p.textContent = 'Ryan is the champion'
+// newHeader.id.append('p')
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Ryan is the champion'
+document.body.appendChild(newHeader)
+
+
+// // let div = document.createElement("div")
+// // div.append("Some text")
+
+// // console.log(div.textContent) // "Some text"
+
+
+
+
